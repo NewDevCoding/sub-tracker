829392596a4442ced48c7a4f53492fedb2415160
Isaac <newdevcoding@gmail.com>
2025-02-23 16:11:36 -0500

    changed auth routes to instead execute the correct functions, defined those functions in auth.controllers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,23 +2,13 @@ import { Router } from "express";
 
 const authRouter = Router();
 
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
-authRouter.post('/sign-up', (req, res) => {
-    res.send({
-        title: 'Sign up'
-    });
-})
 
-authRouter.post('/sign-in', (req, res) => {
-    res.send({
-        title: 'Sign in'
-    });
-})
+authRouter.post('/sign-up', signUp)
 
-authRouter.post('/sign-out', (req, res) => {
-    res.send({
-        title: 'Sign out'
-    });
-})
+authRouter.post('/sign-in', signIn)
+
+authRouter.post('/sign-out', signOut)
 
 export default authRouter;
