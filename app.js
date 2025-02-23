d12d2460102e3237f4df6bcc1db4c603c2618afc
Isaac <newdevcoding@gmail.com>
2025-02-23 14:59:25 -0500

    added three routers to use app for usage

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 
+import { PORT } from './config/env.js';
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscriptions.routes.js';
+
 const app = express();
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);  
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello Worl');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
+
+export default app;
