18ff23e37ccb7d6b31807c9aa4bd52a44feeb3db
Isaac <newdevcoding@gmail.com>
2025-02-23 15:45:47 -0500

    implemented usage of error middleware in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscriptions.routes.js';
 
 import connectToDatabase from './database/mongoose.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 
 const app = express();
@@ -14,6 +15,8 @@ const app = express();
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);  
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Hello Worl');
