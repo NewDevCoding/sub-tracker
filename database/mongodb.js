46fe7b51ea91b9196fe69317694ba35d37ab29ef
Isaac <newdevcoding@gmail.com>
2025-02-23 15:10:10 -0500

    created connect to db function

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-import { DB_URI, NODE_ENV} from "../config/env";
+import { DB_URI, NODE_ENV } from "../config/env";
 
 if(!DB_URI){
     throw new Error("Please define the DB_URI in the .env file");
@@ -8,10 +8,14 @@ if(!DB_URI){
 
 const connectToDatabase = async () => {
     try {
-        
+        await mongoose.connect(DB_URI);
+
+        console.log(`Connected to the database in ${NODE_ENV} mode`);
     } catch (error) {
         console.log("Error connecting to the database", error);
 
         process.exit(1);
     }
 }
+
+export default connectToDatabase;
